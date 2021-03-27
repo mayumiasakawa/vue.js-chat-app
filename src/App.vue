f2af25de893db49acabf9af6c444170c5dd5113b
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-27 17:05:14 +0900

    add to templete

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+      <h1>Chat App</h1>
+      <!-- ログイン時にはフォームログアウトボタンを表示 -->
+      <div v-if="user.uid" key="login">
+        {{ user.displayName }}
+        <button type="button" @click="doLogout">ログアウト</button>
+      </div>
+      <!-- 未ログイン時にはログインボタンを表示 -->
+      <div v-else key="logout">
+        <button type="button" @click="doLogin">ログイン</button>
+      </div>
+    </header>
+
+    <!-- Firebaseから取得したリストを描画（トランジション付き） -->
+    <transition-group name="chat" tag="div" class="list content">
+      <section v-for="{ key, name, image, message} in chat" :key="key" class="item">
+        <div class="item-image"><img :src="image" width="40" height="40"></div>
+        <div class="item-detail">
+          <div class="item-name">{{ name }}</div>
+          <div class="item-message">
+            <nl2br tag="div" :text="message">
+          </div>
+        </div>
+      </section>
+    </transition-group>
+
+  <!-- 入力フォーム -->
+  <form action="" @submit.prevent="doSend" class="form">
+    <textarea
+      v-model="input"
+      :disabled="!user.uid"
+      @keydown.enter.exact.prevent="doSend"></textarea>
+    <button type="submit" :disabled="!user.uid" class="send-button">Send</button>
+  </form>
   </div>
 </template>
 
